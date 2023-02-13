dfdf9978875b630736db2d4ae5bebd07866f5f56
Oluwadarasimi Odejobi <oluwadarasimi5000@gmail.com>
2023-02-13 22:22:47 +0100

    first commit for this project

---
+++ script.js
@@ -0,0 +1,14 @@
+// fetch("data.json")
+//   .then(function (response) {
+//     return response.json();
+//   })
+
+//   .then(function (data) {
+//     let placeholder = document.querySelector("#data-output");
+//     let out = "";
+//     for (let info of products) {
+//       out += `
+
+//       `;
+//     }
+//   })
