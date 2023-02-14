479798653a58a19d7c82f50a18310f02c1410e56
Oluwadarasimi Odejobi <oluwadarasimi5000@gmail.com>
2023-02-14 22:54:14 +0100

    done with the design and filtering with javascript

--- script.js
+++ script.js
@@ -12,3 +12,383 @@
 //       `;
 //     }
 //   })
+
+// const listingOne = document.getElementById("listing-I");
+// const listingTwo = document.getElementById("listing-II");
+// const listingThree = document.getElementById("listing-III");
+// const listingFour = document.getElementById("listing-IV");
+// const listingFive = document.getElementById("listing-V");
+// const listingSix = document.getElementById("listing-VI");
+// const listingSeven = document.getElementById("listing-VII");
+// const listingEight = document.getElementById("listing-VIII");
+// const listingNine = document.getElementById("listing-IX");
+// const listingTen = document.getElementById("listing-X");
+// const listing = document.querySelector(".listing");
+// const frontendTag = document.querySelector(".frontend-tag");
+// const fullstackTag = document.querySelector(".fullstack-tag");
+// const backendTag = document.querySelector(".backend-tag");
+// const juniorTag = document.querySelector(".junior-tag");
+// const midweightTag = document.querySelector(".midweight-tag");
+// const seniorTag = document.querySelector(".senior-tag");
+
+// frontendTag.addEventListener("click", function () {
+//   if (listing.dataset.role === "frontend") {
+//     listing.classList.add = "frontend"
+//   }
+// });
+
+// frontendTag.addEventListener("click", function () {
+// if (listingOne.classList.contains("fullstack") || listingOne.classList.contains("backend")) {
+//     listingOne.style.display = "none";
+//   }
+
+//   if (listingTwo.classList.contains("fullstack") || listingTwo.classList.contains("backend")) {
+//     listingTwo.style.display = "none";
+//   }
+
+//   if (listingThree.classList.contains("fullstack") || listingThree.classList.contains("backend")) {
+//     listingThree.style.display = "none";
+//   }
+
+//   if (listingFour.classList.contains("fullstack") || listingFour.classList.contains("backend")) {
+//     listingFour.style.display = "none";
+//   }
+
+//   if (listingFive.classList.contains("fullstack") || listingFive.classList.contains("backend")) {
+//     listingFive.style.display = "none";
+//   }
+
+//   if (listingSix.classList.contains("fullstack") || listingSix.classList.contains("backend")) {
+//     listingSix.style.display = "none";
+//   }
+
+//   if (listingSeven.classList.contains("fullstack") || listingSeven.classList.contains("backend")) {
+//     listingSeven.style.display = "none";
+//   }
+
+//   if (listingEight.classList.contains("fullstack") || listingEight.classList.contains("backend")) {
+//     listingEight.style.display = "none";
+//   }
+
+//   if (listingNine.classList.contains("fullstack") || listingNine.classList.contains("backend")) {
+//     listingNine.style.display = "none";
+//   }
+
+//   if (listingTen.classList.contains("fullstack") || listingTen.classList.contains("backend")) {
+//     listingTen.style.display = "none";
+//   }
+// });
+
+// fullstackTag.addEventListener("click", function () {
+//   if (listingOne.classList.contains("frontend") || listingOne.classList.contains("backend")) {
+//     listingOne.style.display = "none";
+//   }
+
+//   if (listingTwo.classList.contains("frontend") || listingTwo.classList.contains("backend")) {
+//     listingTwo.style.display = "none";
+//   }
+
+//   if (listingThree.classList.contains("frontend") || listingThree.classList.contains("backend")) {
+//     listingThree.style.display = "none";
+//   }
+
+//   if (listingFour.classList.contains("frontend") || listingFour.classList.contains("backend")) {
+//     listingFour.style.display = "none";
+//   }
+
+//   if (listingFive.classList.contains("frontend") || listingFive.classList.contains("backend")) {
+//     listingFive.style.display = "none";
+//   }
+
+//   if (listingSix.classList.contains("frontend") || listingSix.classList.contains("backend")) {
+//     listingSix.style.display = "none";
+//   }
+
+//   if (listingSeven.classList.contains("frontend") || listingSeven.classList.contains("backend")) {
+//     listingSeven.style.display = "none";
+//   }
+
+//   if (listingEight.classList.contains("frontend") || listingEight.classList.contains("backend")) {
+//     listingEight.style.display = "none";
+//   }
+
+//   if (listingNine.classList.contains("frontend") || listingNine.classList.contains("backend")) {
+//     listingNine.style.display = "none";
+//   }
+
+//   if (listingTen.classList.contains("frontend") || listingTen.classList.contains("backend")) {
+//     listingTen.style.display = "none";
+//   }
+// });
+
+// backendTag.addEventListener("click", function () {
+//   if (listingOne.classList.contains("frontend") || listingOne.classList.contains("fullstack")) {
+//     listingOne.style.display = "none";
+//   }
+
+//   if (listingTwo.classList.contains("frontend") || listingTwo.classList.contains("fullstack")) {
+//     listingTwo.style.display = "none";
+//   }
+
+//   if (listingThree.classList.contains("frontend") || listingThree.classList.contains("fullstack")) {
+//     listingThree.style.display = "none";
+//   }
+
+//   if (listingFour.classList.contains("frontend") || listingFour.classList.contains("fullstack")) {
+//     listingFour.style.display = "none";
+//   }
+
+//   if (listingFive.classList.contains("frontend") || listingFive.classList.contains("fullstack")) {
+//     listingFive.style.display = "none";
+//   }
+
+//   if (listingSix.classList.contains("frontend") || listingSix.classList.contains("fullstack")) {
+//     listingSix.style.display = "none";
+//   }
+
+//   if (listingSeven.classList.contains("frontend") || listingSeven.classList.contains("fullstack")) {
+//     listingSeven.style.display = "none";
+//   }
+
+//   if (listingEight.classList.contains("frontend") || listingEight.classList.contains("fullstack")) {
+//     listingEight.style.display = "none";
+//   }
+
+//   if (listingNine.classList.contains("frontend") || listingNine.classList.contains("fullstack")) {
+//     listingNine.style.display = "none";
+//   }
+
+//   if (listingTen.classList.contains("frontend") || listingTen.classList.contains("fullstack")) {
+//     listingTen.style.display = "none";
+//   }
+// });
+
+// juniorTag.addEventListener("click", function () {
+//   if (listingOne.classList.contains("midweight") || listingOne.classList.contains("senior")) {
+//     listingOne.style.display = "none";
+//   }
+
+//   if (listingTwo.classList.contains("midweight") || listingTwo.classList.contains("senior")) {
+//     listingTwo.style.display = "none";
+//   }
+
+//   if (listingThree.classList.contains("midweight") || listingThree.classList.contains("senior")) {
+//     listingThree.style.display = "none";
+//   }
+
+//   if (listingFour.classList.contains("midweight") || listingFour.classList.contains("senior")) {
+//     listingFour.style.display = "none";
+//   }
+
+//   if (listingFive.classList.contains("midweight") || listingFive.classList.contains("senior")) {
+//     listingFive.style.display = "none";
+//   }
+
+//   if (listingSix.classList.contains("midweight") || listingSix.classList.contains("senior")) {
+//     listingSix.style.display = "none";
+//   }
+
+//   if (listingSeven.classList.contains("midweight") || listingSeven.classList.contains("senior")) {
+//     listingSeven.style.display = "none";
+//   }
+
+//   if (listingEight.classList.contains("midweight") || listingEight.classList.contains("senior")) {
+//     listingEight.style.display = "none";
+//   }
+
+//   if (listingNine.classList.contains("midweight") || listingNine.classList.contains("senior")) {
+//     listingNine.style.display = "none";
+//   }
+
+//   if (listingTen.classList.contains("midweight") || listingTen.classList.contains("senior")) {
+//     listingTen.style.display = "none";
+//   }
+// });
+
+// const listing = document.querySelectorAll(".listing")
+// const frontendTag = document.querySelectorAll(".frontend-tag");
+
+// for (let i = 0; i < frontendTag.length; i++) {
+//   frontendTag[i].addEventListener("click", function () {
+//     listing[i].classList.toggle("red");
+//   });
+// }
+
+const listingOne = document.getElementById("listing-I");
+const listingTwo = document.getElementById("listing-II");
+const listingThree = document.getElementById("listing-III");
+const listingFour = document.getElementById("listing-IV");
+const listingFive = document.getElementById("listing-V");
+const listingSix = document.getElementById("listing-VI");
+const listingSeven = document.getElementById("listing-VII");
+const listingEight = document.getElementById("listing-VIII");
+const listingNine = document.getElementById("listing-IX");
+const listingTen = document.getElementById("listing-X");
+// const listing = document.querySelector(".listing");
+// const frontendTag = document.querySelector(".frontend-tag");
+// const fullstackTag = document.querySelector(".fullstack-tag");
+// const backendTag = document.querySelector(".backend-tag");
+// const juniorTag = document.querySelector(".junior-tag");
+// const midweightTag = document.querySelector(".midweight-tag");
+// const seniorTag = document.querySelector(".senior-tag");
+
+function frontendTag() {
+  listingTwo.classList.add("fullstack", "backend");
+  listingFive.classList.add("fullstack", "backend");
+  listingSix.classList.add("fullstack", "backend");
+  listingNine.classList.add("fullstack", "backend");
+}
+
+function fullstackTag() {
+  listingOne.classList.add("frontend", "backend");
+  listingThree.classList.add("frontend", "backend");
+  listingFour.classList.add("frontend", "backend");
+  listingSix.classList.add("frontend", "backend");
+  listingSeven.classList.add("frontend", "backend");
+  listingEight.classList.add("frontend", "backend");
+  listingTen.classList.add("frontend", "backend");
+}
+
+function backendTag() {
+  listingOne.classList.add("frontend", "fullstack");
+  listingTwo.classList.add("frontend", "fullstack");
+  listingThree.classList.add("frontend", "fullstack");
+  listingFour.classList.add("frontend", "fullstack");
+  listingFive.classList.add("frontend", "fullstack");
+  listingSeven.classList.add("frontend", "fullstack");
+  listingEight.classList.add("frontend", "fullstack");
+  listingNine.classList.add("frontend", "fullstack");
+  listingTen.classList.add("frontend", "fullstack");
+}
+
+function juniorTag() {
+  listingOne.classList.add("midweight", "senior");
+  listingTwo.classList.add("midweight", "senior");
+  listingFive.classList.add("midweight", "senior");
+  listingNine.classList.add("midweight", "senior");
+}
+
+function midweightTag() {
+  listingOne.classList.add("junior", "senior");
+  listingThree.classList.add("junior", "senior");
+  listingFour.classList.add("junior", "senior");
+  listingSix.classList.add("junior", "senior");
+  listingSeven.classList.add("junior", "senior");
+  listingEight.classList.add("junior", "senior");
+  listingTen.classList.add("junior", "senior");
+}
+
+function seniorTag() {
+  listingTwo.classList.add("junior", "midweight");
+  listingThree.classList.add("junior", "midweight");
+  listingFour.classList.add("junior", "midweight");
+  listingFive.classList.add("junior", "midweight");
+  listingSix.classList.add("junior", "midweight");
+  listingSeven.classList.add("junior", "midweight");
+  listingEight.classList.add("junior", "midweight");
+  listingNine.classList.add("junior", "midweight");
+  listingTen.classList.add("junior", "midweight");
+}
+
+function pythonTag() {
+  listingOne.classList.add("ruby", "javascript", "html", "css");
+  listingThree.classList.add("ruby", "javascript", "html", "css");
+  listingFour.classList.add("ruby", "javascript", "html", "css");
+  listingFive.classList.add("ruby", "javascript", "html", "css");
+  listingSix.classList.add("ruby", "javascript", "html", "css");
+  listingSeven.classList.add("ruby", "javascript", "html", "css");
+  listingEight.classList.add("ruby", "javascript", "html", "css");
+  listingTen.classList.add("ruby", "javascript", "html", "css");
+}
+
+function rubyTag() {
+  listingOne.classList.add("python", "javascript", "html", "css");
+  listingTwo.classList.add("python", "javascript", "html", "css");
+  listingThree.classList.add("python", "javascript", "html", "css");
+  listingFour.classList.add("python", "javascript", "html", "css");
+  listingSeven.classList.add("python", "javascript", "html", "css");
+  listingEight.classList.add("python", "javascript", "html", "css");
+  listingNine.classList.add("python", "javascript", "html", "css");
+  listingTen.classList.add("python", "javascript", "html", "css");
+}
+
+function javascriptTag() {
+  listingTwo.classList.add("ruby", "python", "html", "css");
+  listingSix.classList.add("ruby", "python", "html", "css");
+}
+
+function htmlTag() {
+  listingTwo.classList.add("ruby", "javascript", "python", "css");
+  listingThree.classList.add("ruby", "javascript", "python", "css");
+  listingFour.classList.add("ruby", "javascript", "python", "css");
+  listingFive.classList.add("ruby", "javascript", "python", "css");
+  listingSix.classList.add("ruby", "javascript", "python", "css");
+  listingEight.classList.add("ruby", "javascript", "python", "css");
+  listingNine.classList.add("ruby", "javascript", "python", "css");
+  listingTen.classList.add("ruby", "javascript", "python", "css");
+}
+
+function cssTag() {
+  listingTwo.classList.add("ruby", "javascript", "html", "python");
+  listingThree.classList.add("ruby", "javascript", "html", "python");
+  listingFive.classList.add("ruby", "javascript", "html", "python");
+  listingSix.classList.add("ruby", "javascript", "html", "python");
+  listingSeven.classList.add("ruby", "javascript", "html", "python");
+  listingEight.classList.add("ruby", "javascript", "html", "python");
+  listingNine.classList.add("ruby", "javascript", "html", "python");
+  listingTen.classList.add("ruby", "javascript", "html", "python");
+}
+
+function reactTag() {
+  listingOne.classList.add("sass", "vue", "django", "ror");
+  listingFour.classList.add("sass", "vue", "django", "ror");
+  listingFive.classList.add("sass", "vue", "django", "ror");
+  listingSix.classList.add("sass", "vue", "django", "ror");
+  listingSeven.classList.add("sass", "vue", "django", "ror");
+  listingEight.classList.add("sass", "vue", "django", "ror");
+  listingNine.classList.add("sass", "vue", "django", "ror");
+}
+
+function sassTag() {
+  listingOne.classList.add("react", "vue", "django", "ror");
+  listingTwo.classList.add("react", "vue", "django", "ror");
+  listingFour.classList.add("react", "vue", "django", "ror");
+  listingSix.classList.add("react", "vue", "django", "ror");
+  listingNine.classList.add("react", "vue", "django", "ror");
+}
+
+function vueTag() {
+  listingOne.classList.add("react", "sass", "django", "ror");
+  listingTwo.classList.add("react", "sass", "django", "ror");
+  listingThree.classList.add("react", "sass", "django", "ror");
+  listingFour.classList.add("react", "sass", "django", "ror");
+  listingFive.classList.add("react", "sass", "django", "ror");
+  listingSix.classList.add("react", "sass", "django", "ror");
+  listingSeven.classList.add("react", "sass", "django", "ror");
+  listingNine.classList.add("react", "sass", "django", "ror");
+  listingTen.classList.add("react", "sass", "django", "ror");
+}
+
+function djangoTag() {
+  listingOne.classList.add("react", "sass", "vue", "ror");
+  listingTwo.classList.add("react", "sass", "vue", "ror");
+  listingThree.classList.add("react", "sass", "vue", "ror");
+  listingFour.classList.add("react", "sass", "vue", "ror");
+  listingFive.classList.add("react", "sass", "vue", "ror");
+  listingSix.classList.add("react", "sass", "vue", "ror");
+  listingSeven.classList.add("react", "sass", "vue", "ror");
+  listingEight.classList.add("react", "sass", "vue", "ror");
+  listingTen.classList.add("react", "sass", "vue", "ror");
+}
+
+function rorTag() {
+  listingOne.classList.add("react", "sass", "vue", "django");
+  listingTwo.classList.add("react", "sass", "vue", "django");
+  listingThree.classList.add("react", "sass", "vue", "django");
+  listingFour.classList.add("react", "sass", "vue", "django");
+  listingFive.classList.add("react", "sass", "vue", "django");
+  listingSeven.classList.add("react", "sass", "vue", "django");
+  listingEight.classList.add("react", "sass", "vue", "django");
+  listingNine.classList.add("react", "sass", "vue", "django");
+  listingTen.classList.add("react", "sass", "vue", "django");
+}
